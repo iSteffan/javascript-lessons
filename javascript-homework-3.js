a9166ee2d820b058bba2f239c31ca5d2a731b4dd
Stepan Skichko <st.skichko@gmail.com>
2023-01-02 17:37:53 +0200

    tasks 1-9 DONE

---
+++ javascript-homework-3.js
@@ -0,0 +1,19 @@
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ["premium", "promoted", "top", "trusted"],
+//   owner: {
+//     name: "Henry Sibola",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location;
+// apartment.location = "Jamaica";
+// // apartment.location.city = "Kingston";
