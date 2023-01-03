fbc98fd956d1f83236966489acf6141972580f26
Stepan Skichko <st.skichko@gmail.com>
2023-01-03 15:06:43 +0200

    homework tasks ...-20 DONE

--- javascript-homework-3.js
+++ javascript-homework-3.js
@@ -18,15 +18,62 @@
 // apartment.location = "Jamaica";
 // // apartment.location.city = "Kingston";
 
-const apartment = {
-  descr: "Spacious apartment in the city center",
-  rating: 4,
-  price: 2153,
-};
-const keys = [];
-const values = [];
-// Change code below this line`
-for (const key in apartment) {
-//   keys += apartment.key;
-//   values += apartment[key]
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Change code below this line`
+// for (const key in apartment) {
+// //   keys += apartment.key;
+// //   values += apartment[key]
+// }
+
+
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   const res = [];
+//   for (product of products) {
+//     if (product[propName] === undefined) {
+//       return res;
+//     }
+//     res.push(product[propName]);
+//   }
+//   return res;
+//   // Change code above this line
+// }
+
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  // Change code below this line
+  let totalPrice = 0;
+for (let product of products) {
+  if (product.name === productName) {
+    totalPrice = product.price * product.quantity
+  }
+  
 }
+
+return totalPrice;
+  // Change code above this line
+}
+
+const a = calculateTotalPrice("Radar")
+console.log(a)
