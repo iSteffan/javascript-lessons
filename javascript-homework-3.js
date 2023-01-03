d3c2adb659f14f18112ce37fe51925fc4f16a655
Stepan Skichko <st.skichko@gmail.com>
2023-01-03 13:22:47 +0200

    little update

--- javascript-homework-3.js
+++ javascript-homework-3.js
@@ -17,3 +17,16 @@
 // apartment.location;
 // apartment.location = "Jamaica";
 // // apartment.location.city = "Kingston";
+
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+const keys = [];
+const values = [];
+// Change code below this line`
+for (const key in apartment) {
+//   keys += apartment.key;
+//   values += apartment[key]
+}
